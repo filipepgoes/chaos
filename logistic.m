553e3c596ebfb3ca47c0d8453fd1ce39dc8db79b
Filipe Goes <filipe.goes@previc.gov.br>
2020-04-02 21:58:22 -0300

    Added logistic.

---
+++ logistic.m
@@ -0,0 +1,35 @@
+function logistic
+  
+  niter=100;
+  x0=0.3;
+  a=[1:0.001:4];
+  p=zeros(length(a),niter);
+  for j=1:1:length(a)
+    y=zeros(1,niter);
+    y(1)=x0;
+    for i=2:1:niter
+      y(i)=g(y(i-1),a(j));
+    endfor
+	y;
+    cp=poles(y);
+    for k=1:1:length(cp)
+      p(j,k)=cp(k);
+    endfor
+  endfor
+  plot(p)
+  ylabel('a')
+  xlabel('poles')
+endfunction
+function y=g(x,a)
+  y=a*x*(1-x);
+endfunction
+function p=poles(y)
+  p=y;
+  for w=1:1:floor(length(y)/2)
+    d=sum(y(length(y)-w:end).-y(length(y)-w-w-1:length(y)-w-1));
+    if (d<=0.01)
+      p=y(length(y)-w:length(y));
+      break
+    endif
+  endfor
+endfunction
